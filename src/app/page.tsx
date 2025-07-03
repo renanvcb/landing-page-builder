d8a366291ddf2c28d3787327097ab07505d45653
Renan Borges <renanvcb@gmail.com>
2025-07-02 23:35:40 -0300

    Change Sidebar import to named export

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
-import Sidebar from "@/components/Sidebar/Sidebar";
+import { Sidebar } from "@/components/Sidebar/Sidebar";
 import styles from "@/styles/page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Sidebar />
+      {/* <Canvas /> */}
     </div>
   );
 }
