2469d18fb43f87a511a3acb9d3691403849def02
Renan Borges <renanvcb@gmail.com>
2025-07-02 23:34:04 -0300

    Export DndWrapper as a named export instead of default export

--- src/components/DnDWrapper.tsx
+++ src/components/DnDWrapper.tsx
@@ -4,6 +4,6 @@ import { ReactNode } from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-export default function DndWrapper({ children }: { children: ReactNode }) {
+export function DndWrapper({ children }: { children: ReactNode }) {
   return <DndProvider backend={HTML5Backend}>{children}</DndProvider>;
 }
