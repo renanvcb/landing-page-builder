98bcb7852732715bb80bfa8d00662d8e3a559531
Renan Borges <renanvcb@gmail.com>
2025-07-08 20:52:34 -0300

    Add support for mobile and desktop drag-and-drop using react-device-detect

--- src/components/DnDWrapper.tsx
+++ src/components/DnDWrapper.tsx
@@ -2,8 +2,22 @@
 
 import { ReactNode } from "react";
 import { DndProvider } from "react-dnd";
+import { isMobile } from "react-device-detect";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { TouchBackend } from "react-dnd-touch-backend";
 
 export function DndWrapper({ children }: { children: ReactNode }) {
-  return <DndProvider backend={HTML5Backend}>{children}</DndProvider>;
+  const options = {
+    enableMouseEvents: true, // Enable mouse events for desktop compatibility
+    enableTouchEvents: true, // Enable touch events for mobile compatibility
+  };
+
+  return (
+    <DndProvider
+      backend={isMobile ? TouchBackend : HTML5Backend}
+      options={options}
+    >
+      {children}
+    </DndProvider>
+  );
 }
