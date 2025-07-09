bcdb08404a1b4dbf443866ddc0d4370621a0cb74
Renan Borges <renanvcb@gmail.com>
2025-07-09 01:44:38 -0300

    Remove unused block components: ButtonBlock, HeadingBlock, ImageBlock, and ParagraphBlock

--- src/components/blocks/ButtonBlock.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import styles from "@/styles/Block.module.css";
-
-export function ButtonBlock() {
-  return <button className={styles.button}>Click Me</button>;
-}
--- src/components/blocks/HeadingBlock.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import styles from "@/styles/Block.module.css";
-
-export function HeadingBlock() {
-  return <h1 className={styles.heading}>Heading Text</h1>;
-}
